7f38005ea8a09ec6e0d917b6483ae151558e84aa
Nizan Dhiaulhaq <nizandhiaulhaq@mail.ugm.ac.id>
2026-01-25 17:30:59 +0700

    feat: Add web app manifest for PWA support and manually construct Farcaster manifest in the well-known route.

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,6 +1,21 @@
-import { withValidManifest } from "@coinbase/onchainkit/minikit";
-import { minikitConfig } from "../../../minikit.config";
+import { minikitConfig } from "@/minikit.config";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  return Response.json(withValidManifest(minikitConfig));
+  const config = {
+    accountAssociation: minikitConfig.accountAssociation,
+    frame: {
+      version: minikitConfig.miniapp.version,
+      name: minikitConfig.miniapp.name,
+      iconUrl: minikitConfig.miniapp.iconUrl,
+      homeUrl: minikitConfig.miniapp.homeUrl,
+      imageUrl: minikitConfig.miniapp.heroImageUrl,
+      buttonTitle: `Launch ${minikitConfig.miniapp.name}`,
+      splashImageUrl: minikitConfig.miniapp.splashImageUrl,
+      splashBackgroundColor: minikitConfig.miniapp.splashBackgroundColor,
+      webhookUrl: minikitConfig.miniapp.webhookUrl,
+    },
+  };
+
+  return NextResponse.json(config);
 }
